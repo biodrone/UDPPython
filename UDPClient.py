ccfa0482939ef480ffc95d70c7d57cda24cc3623
Josh Jacobs <josh.jacobs19@gmail.com>
2015-06-23 11:19:57 +0100

    Server and client now act as they should. Server exits after 4 workers. Need to have a timeout on the client because if the server is off then the client waits indefinitley.

--- UDPClient.py
+++ UDPClient.py
@@ -9,17 +9,12 @@ UDP_IP_R = "127.0.0.1"
 UDP_PORT_R = 13337
 UDP_IP_S = "127.0.0.1"
 UDP_PORT_S = 13338
-MESSAGE = 'Hello, World!'
+MESSAGE = socket.gethostname()
 
-def sender():
+def sender(): # client sender only sends once, on WAN might need more
     print 'Sending...'
-    i = 0
-    while i < 20:
-        sockS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sockS.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
-        time.sleep(1)
-        print i
-        i += 1
+    sockS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sockS.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
     return
 def listener():
 	print 'Listening...'
@@ -28,7 +23,7 @@ def listener():
 	sockR.bind((UDP_IP_R, UDP_PORT_R))
 
   	data, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
-   	print "received message:", data
+   	print "Connected to Controller:", data
 
 s = threading.Thread(name='Sender', target=sender)
 l = threading.Thread(name='Listener', target=listener)
--- UDPServer.py
+++ UDPServer.py
@@ -10,34 +10,37 @@ UDP_IP_S = "127.0.0.1"
 UDP_PORT_S = 13337
 UDP_IP_R = "127.0.0.1"
 UDP_PORT_R = 13338
-MESSAGE = "Hello, World!"
+MESSAGE = socket.gethostname()
+workers = 0
+exit = 0
 
 def sender():
+    global exit
     print 'Sending...'
-    i = 0
-    while i < 20:
+    while exit == 0:
         sockS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sockS.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
         time.sleep(1)
-        print i
-        i += 1
     return
 def listener():
-    print 'Listening...'
-    j = 0
-    sockR = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sockR.bind((UDP_IP_R, UDP_PORT_R))
+    global workers
+    global exit
+    while workers <= 3:
+        print 'Listening...'
+        sockR = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sockR.bind((UDP_IP_R, UDP_PORT_R))
 
+        data, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
+        print "New Worker:", data
+        workers += 1
 
-    data, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
-
-    if data == 1:
-        print 'message = 1'
-    elif data == 2:
-        print 'message = 2'
-    elif data == 3:
-        print 'message = 3'
+        if data == 1:
+            print 'message = 1'
+        elif data == 2:
+            print 'message = 2'
+        elif data == 3:
+            print 'message = 3'
+    exit = 1
     return
 
 s = threading.Thread(name='Sender', target=sender)
