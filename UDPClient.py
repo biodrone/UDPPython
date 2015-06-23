b3784d991fe5ea6db79b68e8fe29bc0158f39c5e
Josh Jacobs <josh.jacobs19@gmail.com>
2015-06-23 10:42:14 +0100

    Client and server now send and receive properly, send/rec done on a seperate thread in each.

--- UDPClient.py
+++ UDPClient.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python
 
 import socket
+import threading
+import time
+import logging
 
 UDP_IP_R = "127.0.0.1"
 UDP_PORT_R = 13337
+UDP_IP_S = "127.0.0.1"
+UDP_PORT_S = 13338
+MESSAGE = 'Hello, World!'
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP_R, UDP_PORT_R))
+def sender():
+    print 'Sending...'
+    i = 0
+    while i < 20:
+        sockS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sockS.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
+        time.sleep(1)
+        print i
+        i += 1
+    return
+def listener():
+	print 'Listening...'
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+	sockR = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sockR.bind((UDP_IP_R, UDP_PORT_R))
 
+  	data, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
+   	print "received message:", data
+
+s = threading.Thread(name='Sender', target=sender)
+l = threading.Thread(name='Listener', target=listener)
+
+s.start()
+l.start()
--- UDPServer.py
+++ UDPServer.py
@@ -1,29 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/pythonS
 
 import threading
 import Queue
 import socket
-
-hash = ""
-pos = 0
+import time
+import logging
 
 UDP_IP_S = "127.0.0.1"
 UDP_PORT_S = 13337
+UDP_IP_R = "127.0.0.1"
+UDP_PORT_R = 13338
 MESSAGE = "Hello, World!"
-message = ''
 
-print "UDP target IP: ", UDP_IP
-print "UDP target port: ", UDP_PORT
-print "Message: ", MESSAGE
+def sender():
+    print 'Sending...'
+    i = 0
+    while i < 20:
+        sockS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sockS.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
+        time.sleep(1)
+        print i
+        i += 1
+    return
+def listener():
+    print 'Listening...'
+    j = 0
+    sockR = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sockR.bind((UDP_IP_R, UDP_PORT_R))
 
-while message != 3:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(MESSAGE, (UDP_IP_S, UDP_PORT_S))
 
-if message == 1:
-    print 'message = 1'
-elif message == 2:
-    print 'message = 2'
-elif message == 3:
-    print 'message = 3'
+    data, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
 
+    if data == 1:
+        print 'message = 1'
+    elif data == 2:
+        print 'message = 2'
+    elif data == 3:
+        print 'message = 3'
+    return
+
+s = threading.Thread(name='Sender', target=sender)
+l = threading.Thread(name='Listener', target=listener)
+
+s.start()
+l.start()
