892d9aff7d6c540d075937bb544fd89fbc4d702f
Josh Jacobs <josh.jacobs19@gmail.com>
2016-08-15 23:43:47 +0100

    adding subparser

--- UDPServer.py
+++ UDPServer.py
@@ -28,7 +28,10 @@ def main(argv):
 
     parser = argparse.ArgumentParser(usage='Break all the things')
     parser.add_argument('URL', help='URL to take config from')
-    parser.add_argument('-s', '--server', help='Run the script in server mode', action='store_true')
+    parser.add_argument()
+    choose = parser.add_subparser('cliserv')
+    cliserv.add_argument('-s', '--server', help='Run the script in server mode', action='store_true')
+    cliserv.add_argument('-c', '--client', help='Run the script in client mode', action='store_true')
 
     args = parser.parse_args()
     url = args.URL
